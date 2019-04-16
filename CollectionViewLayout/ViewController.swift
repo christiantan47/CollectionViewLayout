306d790fa4cbe245a5fa6d7bd2e40ce84bb0e22c
Livia Kartika <liviakartika@hotmail.com>
2019-04-16 13:05:32 +0700

    Initial Commit

---
+++ CollectionViewLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewLayout
+//
+//  Created by Tan Christian on 16/04/19.
+//  Copyright © 2019 Tan Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
