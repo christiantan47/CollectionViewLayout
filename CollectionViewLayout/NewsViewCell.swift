ec9e5445bc26fcbd4713004a0c4ce25f1abdb46c
Livia Kartika <liviakartika@hotmail.com>
2019-04-17 14:25:33 +0700

    collectionview basic

---
+++ CollectionViewLayout/NewsViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsViewCell.swift
+//  CollectionViewLayout
+//
+//  Created by Tan Christian on 16/04/19.
+//  Copyright © 2019 Tan Christian. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var newsLabel: UILabel!
+}
--- CollectionViewLayout/ViewController.swift
+++ CollectionViewLayout/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newsCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newsCollection.delegate = self
+        newsCollection.dataSource = self
     }
 
 
+}
+
+extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = newsCollection.dequeueReusableCell(withReuseIdentifier: "NewsCell", for: indexPath) as! NewsViewCell
+        cell.newsLabel.text = "Berita \(indexPath.row)"
+        return cell
+    }
+    
 }
 
