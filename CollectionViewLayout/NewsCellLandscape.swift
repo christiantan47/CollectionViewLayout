aa54d743bc42c6c1d87cced7436d41d972f1e941
Livia Kartika <liviakartika@hotmail.com>
2019-04-22 15:03:42 +0700

    new xib for landscape

---
+++ CollectionViewLayout/NewsCellLandscape.swift
@@ -0,0 +1,22 @@
+//
+//  NewsCellLandscape.swift
+//  CollectionViewLayout
+//
+//  Created by Tan Christian on 22/04/19.
+//  Copyright © 2019 Tan Christian. All rights reserved.
+//
+
+import UIKit
+
+class NewsCellLandscape: UICollectionViewCell {
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsSnippetLabel: UILabel!
+    @IBOutlet weak var newsDateLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
