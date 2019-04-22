254f2bae270cee236bca06b15c5bc057cc246b2c
Hubert Wang <hubert.tatra@gmail.com>
2019-04-22 19:42:42 +0700

    Add Helper raw file

---
+++ CollectionViewLayout/Helper/Helper.swift
@@ -0,0 +1,10 @@
+//
+//  Helper.swift
+//  CollectionViewLayout
+//
+//  Created by Hubert Wang on 22/04/19.
+//  Copyright © 2019 Tan Christian. All rights reserved.
+//
+
+import Foundation
+import UIKit
