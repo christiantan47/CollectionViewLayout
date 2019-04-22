a36816c0ca5e6dfad20265129ea9b1ca0a089274
Hubert Wang <hubert.tatra@gmail.com>
2019-04-22 19:58:16 +0700

    Add Helper singleton + add fetch image function (in callback)

--- CollectionViewLayout/Helper/Helper.swift
+++ CollectionViewLayout/Helper/Helper.swift
@@ -8,3 +8,38 @@
 
 import Foundation
 import UIKit
+
+class Helper {
+    static let shared = Helper()
+    
+    private init() {}
+    
+    /**
+     Fetch image from using a URL. Make sure Internet connection is active. Completion handler runs in background thread asynchronously (need to Dispatch and put the task into main thread)
+     
+     - Parameters:
+         - url: A valid URL of type String
+         - completion: A normal escaping completion handler with UIImage and Error
+     
+     - Return: None
+    */
+    func fetchImage(from url: String, _ completion: @escaping((UIImage?, Error?) -> Void)) {
+        guard let url = URL(string: url) else {
+            completion(nil, (Error.self as! Error))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
+            completion(UIImage(data: data), nil)
+        }.resume()
+        
+    }
+}
